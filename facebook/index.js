a7eb02d7bf57e3f9cdb487b520581d548fccf5c7
Tejasree-123 <129086080+Tejasree-123@users.noreply.github.com>
2023-05-23 20:51:31 +0530

    Add files via upload

---
+++ facebook/index.js
@@ -0,0 +1,23 @@
+.home{
+    padding: 10px 0 0;
+    display: flex;
+    float: left;
+    margin-left: 20px;
+    <div class="text">
+            <a href="index.html">Home</a><br>
+            
+            <a href="live.html">Live</a><br>
+            <a href="#">Reels</a><br>
+            <a href="shows.html">shows</a> <br>
+            <a href="savedvideos.html">savedvideos</a><br>
+        </div>
+        <div class="text">
+          <ul>
+              <li><a href="#"><img src="homeicon.svg"height="30px" width="30px" top=50px>Home</a></li>
+              <li><a href="#"><img src="live1.jpg" height="30px" width="30px" top=50px>Live</a></li>
+                <li><a href="#"><img src="reels.png"height="30px" width="30px" top=50px>Reels</a></li>
+                <li><a href="shows.html">shows</a></li>
+                <li><a href="savedvideos.html">savedvideos</a></li>
+                </ul>
+          </div>
+    
